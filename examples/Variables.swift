da0ea38ae8566e9598dc11e784cdce1be6ab2f2c
emanuelefavero <emafaux89@gmail.com>
2024-08-07 23:06:14 +0200

    add examples/Variables

---
+++ examples/Variables.swift
@@ -0,0 +1,14 @@
+// Variables
+var name = "John"
+print("Hello \(name)")
+name = "Jane"
+print("Hello \(name)")
+
+// Constants
+let pi = 3.14159
+print("The value of pi is \(pi)")
+
+// Type annotations
+// Swift can infer the type of a variable, but you can also specify it explicitly:
+var age: Int = 30
+print("I am \(age) years old")
