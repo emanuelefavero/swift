4c3ba33f6da1b2b6adc868ae5ce54ae207a39668
emanuelefavero <emafaux89@gmail.com>
2024-08-12 22:59:41 +0200

    add examples/OOP/Methods

---
+++ examples/OOP/Methods.swift
@@ -0,0 +1,31 @@
+// * Methods
+// Instance methods are functions that belong to instances of a particular class, structure, or enumeration
+
+struct User {
+  var name: String = "John"
+
+  // Methods are defined inside the structure using the func keyword
+  func greet() -> String {
+    return "Hi \(name)"
+  }
+}
+
+var user: User = User()
+print(user.greet()) // Hi John
+
+// * The self Property
+// The self property refers to the current instance of the type
+
+struct Counter {
+  var num: Int = 0
+
+  // TIP: The self property is particularly useful when a parameter name is the same as a property name
+  // ? mutating keyword is used to indicate that the method is allowed to modify the properties of the structure. By default, the properties of a value type cannot be modified from within its instance methods
+  mutating func increment(num: Int) {
+    self.num += num
+  }
+}
+
+var counter: Counter = Counter()
+counter.increment(num: 10)
+print(counter.num) // 10
